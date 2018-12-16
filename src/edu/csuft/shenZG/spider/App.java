76846418320ed6b45f8c57399fe06e7ad83d5fd7
zhiguangshen <45841312+zhiguangshen@users.noreply.github.com>
2018-12-16 17:21:47 +0800

    解析影片的信息

--- src/edu/csuft/shenZG/spider/App.java
+++ src/edu/csuft/shenZG/spider/App.java
@@ -1,5 +1,57 @@
 package edu.csuft.shenZG.spider;
 
+import java.io.IOException;
+import java.util.ArrayList;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+/*
+ * 
+ * @auther shenZG
+ * 
+ */
 public class App {
+	// alt/
+	public static void main(String[] args) {
+		//目标 URL
+		String url = "https://movie.douban.com/top250";
+		
+		//抓取jsoup获取文档
+		try {
+			Document doc = Jsoup.connect(url).get();
+			
+			Elements es = doc.select(".grid_view .item");
+			System.out.println(es.size());
+			
+			// 创建一个影片列表
+			ArrayList<Film> list = new ArrayList();
+			
+			
+			for(Element e : es) {
+				Film f = new Film();
+				
+				Element t = e.select(".title").first();
+				String num = e.select(".star span").last().text();
+				System.out.println(t.text() + "," + num);
+				
+				// f.id
+				//f.title
+				list.add(f);
+			}
+			
+//			String title = doc.title();
+//			String data =doc.data();
+//			
+//			System.out.println(title);
+//			System.out.println(data);
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 
 }
---
+++ src/edu/csuft/shenZG/spider/Film.java
@@ -0,0 +1,35 @@
+package edu.csuft.shenZG.spider;
+
+/*
+ * @auhter shenZG
+ * 
+ * 影片
+ */
+public class Film {
+
+	/*
+	 * 影片名称
+	 */
+	String title;
+	/*
+	 * 相关信息
+	 */
+	String info;
+	/*
+	 * 评分
+	 */
+	double rating;
+	/*
+	 * 评分人数
+	 */
+	int num;
+	/*
+	 * 影片排名
+	 */
+	int id;
+	/*
+	 * 影片海报
+	 */
+	String poster;
+	
+}
